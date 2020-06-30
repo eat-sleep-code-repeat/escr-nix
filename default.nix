6c10d4441b76caf61e5b2c663ca875e94c2d5c3b
Philip Beadle <philip.beadle@live.com.au>
2020-06-30 12:30:55 +1000

    Trying to add dromne and gittea

--- default.nix
+++ default.nix
@@ -2,21 +2,23 @@ let
  pkgs = import ./nixpkgs;
  node = pkgs.callPackage ./node { };
  git = pkgs.callPackage ./git { };
+#  drone = pkgs.callPackage ./drone { };
 
- philt3r-shell = pkgs.callPackage ./nix-shell {
+ escr-shell = pkgs.callPackage ./nix-shell {
   pkgs = pkgs;
   git = git;
   node = node;
+#   drone = drone;
  };
 
  # override and overrideDerivation cannot be handled by mkDerivation
- derivation-safe-philt3r-shell = (removeAttrs philt3r-shell ["override" "overrideDerivation"]);
+ derivation-safe-escr-shell = (removeAttrs escr-shell ["override" "overrideDerivation"]);
 in
 {
  pkgs = pkgs;
  # export the set used to build shell alongside the main derivation
  # downstream devs can extend/override the shell as needed
- # philt3r-shell provides canonical dev shell for generic work
- shell = derivation-safe-philt3r-shell;
- main = pkgs.stdenv.mkDerivation derivation-safe-philt3r-shell;
+ # escr-shell provides canonical dev shell for generic work
+ shell = derivation-safe-escr-shell;
+ main = pkgs.stdenv.mkDerivation derivation-safe-escr-shell;
 }
--- nix-shell/default.nix
+++ nix-shell/default.nix
@@ -1,22 +1,13 @@
+{ pkgs, git, node }:
 {
- pkgs,
- git,
- node
-#  vue
-}:
-{
- name = "philt3r-shell";
-
+ name = "escr-shell";
  buildInputs = [
-  # for mktemp
   pkgs.coreutils
-
-  # simple dev feedback loop
   pkgs.unixtools.watch
  ]
  ++ (pkgs.callPackage ./flush { }).buildInputs
  ++ git.buildInputs
  ++ node.buildInputs
-#  ++ vue.buildInputs
+#  ++ drone.buildInputs
  ;
 }
